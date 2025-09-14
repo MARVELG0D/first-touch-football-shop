a4f34eb8b7066cbccf8ef326cec7a9d3fdcdc9ed
MARVELG0D <bukan.marvel69@gmail.com>
2025-09-14 16:39:55 +0700

    ti3

--- main/views.py
+++ main/views.py
@@ -24,7 +24,7 @@ def add_product(request):
         return redirect('main:show_main')
 
     context = {'form': form}
-    return render(request, "create_product.html", context)
+    return render(request, "add_product.html", context)
 
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
